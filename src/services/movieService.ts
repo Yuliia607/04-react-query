3a23a3feb0cd6369be15c900e7b165ea083252a6
Yuliia Dyrda <yuliia0704@gmail.com>
2026-05-28 21:31:57 +0200

    update

---
+++ src/services/movieService.ts
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+import type { MoviesResponse } from "../types/movie";
+
+const BASE_URL = "https://api.themoviedb.org/3";
+
+const options = {
+  headers: {
+    Authorization: `Bearer ${import.meta.env.VITE_TMDB_TOKEN}`,
+  },
+};
+
+export const fetchMovies = async (
+  query: string,
+  page: number
+): Promise<MoviesResponse> => {
+  const response = await axios.get<MoviesResponse>(
+    `${BASE_URL}/search/movie`,
+    {
+      params: {
+        query,
+        page,
+      },
+      ...options,
+    }
+  );
+
+  return response.data;
+};
